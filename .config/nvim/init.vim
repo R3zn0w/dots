0243d2e09966b075106963c45dea2f87916e8c3d
carbon <>
2025-03-06 11:55:45 +0100

    initial sync

---
+++ .config/nvim/init.vim
@@ -0,0 +1,7 @@
+lua require('config')
+set nu rnu
+set tabstop=4 
+set softtabstop=4
+set autoindent
+set expandtab
+colorscheme kanagawa-dragon
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# placeholder, remember to prompt about wallpaper in ~/.config/usermeta
+# TODO ensure fonts are there, fallbacks would be nice
